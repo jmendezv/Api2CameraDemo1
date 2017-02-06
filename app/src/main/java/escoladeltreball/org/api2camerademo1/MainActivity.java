fb0f565a636f0e849c9c0324a2706556de1bdf88
Pep Mendez <jmendez1@xtec.cat>
2017-02-06 20:37:07 +0100

    on comment state

--- app/src/main/java/escoladeltreball/org/api2camerademo1/MainActivity.java
+++ app/src/main/java/escoladeltreball/org/api2camerademo1/MainActivity.java
@@ -44,26 +44,96 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.LogRecord;
 
 
 public class MainActivity extends AppCompatActivity {
     private static final int PERMISSION_REQUEST = 1;
+    // width/height
     private Size previewsize;
     private Size jpegSizes[] = null;
     private TextureView textureView;
+    /*
+    The CameraDevice class is a representation of a single camera connected to an Android device,
+    allowing for fine-grain control of image capture and post-processing at high frame rates.
+
+    A given camera device may provide support at one of two levels: limited or full.
+    If a device only supports the limited level,
+    then Camera2 exposes a feature set that is roughly equivalent to the older Camera API,
+    although with a cleaner and more efficient interface.
+    Devices that implement the full level of support provide substantially improved capabilities
+    over the older camera API.
+    Applications that target the limited level devices will run unchanged on the full-level devices;
+    if your application requires a full-level device for proper operation,
+    declare the "android.hardware.camera.level.full" feature in your manifest.
+     */
     private CameraDevice cameraDevice;
+    /*
+    To obtain a builder instance, use the createCaptureRequest(int) method from CameraDevice,
+    which initializes the request fields to one of the templates defined in CameraDevice.
+    TEMPLATE_PREVIEW
+    TEMPLATE_RECORD
+    TEMPLATE_STILL_CAPTURE
+    TEMPLATE_VIDEO_SNAPSHOT
+    TEMPLATE_MANUAL
+     */
     private CaptureRequest.Builder previewBuilder;
+    /*
+        A configured capture session for a CameraDevice, used for capturing images from the camera.
+
+     */
     private CameraCaptureSession previewSession;
     private Button getpicture;
-    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    //    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    private static final Map<Integer, Integer> ORIENTATIONS = new HashMap<>();
+    private TextureView.SurfaceTextureListener surfaceTextureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            openCamera();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+        }
+    };
+    private CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(CameraDevice camera) {
+            cameraDevice = camera;
+            startCamera();
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice camera) {
+        }
+
+        @Override
+        public void onError(CameraDevice camera, int error) {
+        }
+    };
 
     static {
-        ORIENTATIONS.append(Surface.ROTATION_0, 90);
-        ORIENTATIONS.append(Surface.ROTATION_90, 0);
-        ORIENTATIONS.append(Surface.ROTATION_180, 270);
-        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+//        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+//        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+//        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+//        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+
+        ORIENTATIONS.put(Surface.ROTATION_0, 90);
+        ORIENTATIONS.put(Surface.ROTATION_90, 0);
+        ORIENTATIONS.put(Surface.ROTATION_180, 270);
+        ORIENTATIONS.put(Surface.ROTATION_270, 180);
     }
 
     @Override
@@ -73,12 +143,26 @@ public class MainActivity extends AppCompatActivity {
         textureView = (TextureView) findViewById(R.id.textureview);
         textureView.setSurfaceTextureListener(surfaceTextureListener);
         getpicture = (Button) findViewById(R.id.getpicture);
-        getpicture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getPicture();
-            }
+        getpicture.setOnClickListener((View view) -> {
+            getPicture();
         });
+    }
+
+    private void save(byte[] bytes) {
+        File file12 = getOutputMediaFile();
+        OutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream(file12);
+            outputStream.write(bytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (outputStream != null)
+                    outputStream.close();
+            } catch (Exception e) {
+            }
+        }
     }
 
     void getPicture() {
@@ -96,7 +180,15 @@ public class MainActivity extends AppCompatActivity {
                 width = jpegSizes[0].getWidth();
                 height = jpegSizes[0].getHeight();
             }
+            /*
+            The ImageReader class allows direct application access to image data rendered into a Surface.
+            Several Android media API classes accept Surface objects as targets to render to,
+            including MediaPlayer, MediaCodec, CameraDevice, ImageWriter and RenderScript Allocations.
+             */
             ImageReader reader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 1);
+            /*
+            TODO
+             */
             List<Surface> outputSurfaces = new ArrayList<Surface>(2);
             outputSurfaces.add(reader.getSurface());
             outputSurfaces.add(new Surface(textureView.getSurfaceTexture()));
@@ -105,40 +197,57 @@ public class MainActivity extends AppCompatActivity {
             capturebuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
             int rotation = getWindowManager().getDefaultDisplay().getRotation();
             capturebuilder.set(CaptureRequest.JPEG_ORIENTATION, ORIENTATIONS.get(rotation));
-            ImageReader.OnImageAvailableListener imageAvailableListener = new ImageReader.OnImageAvailableListener() {
-                @Override
-                public void onImageAvailable(ImageReader reader) {
-                    Image image = null;
-                    try {
-                        image = reader.acquireLatestImage();
-                        ByteBuffer buffer = image.getPlanes()[0].getBuffer();
-                        byte[] bytes = new byte[buffer.capacity()];
-                        buffer.get(bytes);
-                        save(bytes);
-                    } catch (Exception ee) {
-                    } finally {
-                        if (image != null)
-                            image.close();
-                    }
-                }
-
-                void save(byte[] bytes) {
-                    File file12 = getOutputMediaFile();
-                    OutputStream outputStream = null;
-                    try {
-                        outputStream = new FileOutputStream(file12);
-                        outputStream.write(bytes);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    } finally {
-                        try {
-                            if (outputStream != null)
-                                outputStream.close();
-                        } catch (Exception e) {
-                        }
-                    }
+            /*
+            onImageAvailable(ImageReader reader)
+             */
+            ImageReader.OnImageAvailableListener imageAvailableListener = (ImageReader imageReader) -> {
+                Image image = null;
+                try {
+                    image = reader.acquireLatestImage();
+                    ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                    byte[] bytes = new byte[buffer.capacity()];
+                    buffer.get(bytes);
+                    save(bytes);
+                } catch (Exception ee) {
+                } finally {
+                    if (image != null)
+                        image.close();
                 }
             };
+//            ImageReader.OnImageAvailableListener imageAvailableListener = new ImageReader.OnImageAvailableListener() {
+//                @Override
+//                public void onImageAvailable(ImageReader reader) {
+//                    Image image = null;
+//                    try {
+//                        image = reader.acquireLatestImage();
+//                        ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+//                        byte[] bytes = new byte[buffer.capacity()];
+//                        buffer.get(bytes);
+//                        save(bytes);
+//                    } catch (Exception ee) {
+//                    } finally {
+//                        if (image != null)
+//                            image.close();
+//                    }
+//                }
+//
+//                void save(byte[] bytes) {
+//                    File file12 = getOutputMediaFile();
+//                    OutputStream outputStream = null;
+//                    try {
+//                        outputStream = new FileOutputStream(file12);
+//                        outputStream.write(bytes);
+//                    } catch (Exception e) {
+//                        e.printStackTrace();
+//                    } finally {
+//                        try {
+//                            if (outputStream != null)
+//                                outputStream.close();
+//                        } catch (Exception e) {
+//                        }
+//                    }
+//                }
+//            };
             HandlerThread handlerThread = new HandlerThread("takepicture");
             handlerThread.start();
             final Handler handler = new Handler(handlerThread.getLooper());
@@ -232,104 +341,70 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
             manager.openCamera(camerId, stateCallback, null);
-        }catch (Exception e)
-        {
+        } catch (Exception e) {
         }
     }
-    private TextureView.SurfaceTextureListener surfaceTextureListener=new TextureView.SurfaceTextureListener() {
-        @Override
-        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
-            openCamera();
-        }
-        @Override
-        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
-        }
-        @Override
-        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
-            return false;
-        }
-        @Override
-        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
-        }
-    };
-    private CameraDevice.StateCallback stateCallback=new CameraDevice.StateCallback() {
-        @Override
-        public void onOpened(CameraDevice camera) {
-            cameraDevice=camera;
-            startCamera();
-        }
-        @Override
-        public void onDisconnected(CameraDevice camera) {
-        }
-        @Override
-        public void onError(CameraDevice camera, int error) {
-        }
-    };
+
     @Override
     protected void onPause() {
         super.onPause();
-        if(cameraDevice!=null)
-        {
+        if (cameraDevice != null) {
             cameraDevice.close();
         }
     }
-    void  startCamera()
-    {
-        if(cameraDevice==null||!textureView.isAvailable()|| previewsize==null)
-        {
+
+    void startCamera() {
+        if (cameraDevice == null || !textureView.isAvailable() || previewsize == null) {
             return;
         }
-        SurfaceTexture texture=textureView.getSurfaceTexture();
-        if(texture==null)
-        {
+        SurfaceTexture texture = textureView.getSurfaceTexture();
+        if (texture == null) {
             return;
         }
-        texture.setDefaultBufferSize(previewsize.getWidth(),previewsize.getHeight());
-        Surface surface=new Surface(texture);
-        try
-        {
-            previewBuilder=cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-        }catch (Exception e)
-        {
+        texture.setDefaultBufferSize(previewsize.getWidth(), previewsize.getHeight());
+        Surface surface = new Surface(texture);
+        try {
+            previewBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+        } catch (Exception e) {
         }
         previewBuilder.addTarget(surface);
-        try
-        {
+        try {
             cameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
                 @Override
                 public void onConfigured(CameraCaptureSession session) {
-                    previewSession=session;
+                    previewSession = session;
                     getChangedPreview();
                 }
+
                 @Override
                 public void onConfigureFailed(CameraCaptureSession session) {
                 }
-            },null);
-        }catch (Exception e)
-        {
+            }, null);
+        } catch (Exception e) {
         }
     }
-    void getChangedPreview()
-    {
-        if(cameraDevice==null)
-        {
+
+    void getChangedPreview() {
+        if (cameraDevice == null) {
             return;
         }
         previewBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
-        HandlerThread thread=new HandlerThread("changed Preview");
+        HandlerThread thread = new HandlerThread("changed Preview");
         thread.start();
-        Handler handler=new Handler(thread.getLooper());
-        try
-        {
+        Handler handler = new Handler(thread.getLooper());
+        try {
             previewSession.setRepeatingRequest(previewBuilder.build(), null, handler);
-        }catch (Exception e){}
+        } catch (Exception e) {
+        }
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -342,6 +417,7 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
     private static File getOutputMediaFile() {
         File mediaStorageDir = new File(
                 Environment
